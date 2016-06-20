8add303d84ee9ce135739cc1ac7490035e81f534
Mike DeAngelo <revmike@gmail.com>
2016-06-20 15:54:26 -0400

    start building script

---
+++ script.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+service hostapd stop
+# TODO: stop dnsmasq
+
+# TODO: remove wlan0 and eth0 from managed devices in Network Manager
+
+ifconfig wlan0 0.0.0.0/0 up
+iwconfig wlan0 mode managed channel 1
+ifconfig eth0  0.0.0.0/0 up
+brctl delbr br0
+brctl addbr br0
+brctl addif br0 eth0
+iw dev wlan0 set 4addr on
+brctl addif br0 wlan0
+ifconfig br0 172.23.13.1/24 up
+
+service hostapd start
+# TODO: start dnsmasq
+echo '1' > /proc/sys/net/ipv4/ip_forward
+iptables --table nat --append POSTROUTING --out-interface wlan1 -j MASQUERADE -s 172.23.13.0/24
+iptables --append FORWARD -s 172.23.13.0/24 -j ACCEPT
+iptables --append FORWARD -d 172.23.13.0/24 -j ACCEPT -m conntrack --ctstate ESTABLISHED,RELATED -i wlan1
