7c92379b923f006a061e7dca18ef7f72654749f2
Mike DeAngelo <revmike@gmail.com>
2016-06-21 19:10:23 -0400

    working script

--- script.sh
+++ script.sh
@@ -5,13 +5,63 @@ LOCAL_WIRE_NET_DEVICE=eth0
 LOCAL_BRIDGE_NET_DEVICE=br0
 LOCAL_IP_ADDRESS=172.23.13.1/24 
 LOCAL_IP_NETWORK=172.23.13.0/24 
-LOCAL_WIFI_CHANNEL=1
+LOCAL_WIFI_CHANNEL=3
+LOCAL_SSID=farpoint_nomap
+LOCAL_DOMAIN=miked.local
+DHCP_RANGE=172.23.13.16,172.23.13.63,72h
+DHCP_OPTION1=option:router,172.23.13.1
 REMOTE_WIFI_NET_DEVICE=wlan1
 
+generate_hostapd_conf() {
+cat << EOF > /etc/hostapd/hostapd.conf 
+interface=${LOCAL_WIFI_NET_DEVICE}
+bridge=${LOCAL_BRIDGE_NET_DEVICE}
+driver=nl80211
+channel=${LOCAL_WIFI_CHANNEL}
+logger_syslog=-1
+logger_syslog_level=2
+logger_stdout=-1
+logger_stdout_level=2
+debug=0
+ctrl_interface=/var/run/hostapd
+ctrl_interface_group=0
+ssid=${LOCAL_SSID}
+auth_algs=3
+eapol_key_index_workaround=0
+eap_server=0
+wpa=3
+wpa_psk_file=/etc/hostapd/wpa_psk
+wpa_key_mgmt=WPA-PSK
+wpa_pairwise=CCMP
+EOF
+}
+
+generate_dnsmasq_conf() {
+cat << EOF > /etc/dnsmasq.conf 
+interface=${LOCAL_BRIDGE_NET_DEVICE}
+domain-needed
+bogus-priv
+no-resolv
+no-poll
+server=8.8.8.8
+server=8.8.4.4
+local=/${LOCAL_DOMAIN}/
+no-hosts
+addn-hosts=/etc/dnsmasq.d/hosts.conf
+expand-hosts
+domain=miked.local
+dhcp-range=${DHCP_RANGE}
+dhcp-option=${DHCP_OPTION1}
+dhcp-option=46,8           # netbios node type
+EOF
+}
+
 build_bridge() {
-  brctl addbr ${LOCAL_BRIDGE_NET_DEVICE}
   ip addr flush dev ${LOCAL_WIFI_NET_DEVICE}
+  #ip link set ${LOCAL_WIFI_NET_DEVICE} down
   iw dev ${LOCAL_WIFI_NET_DEVICE} set 4addr on
+  #ip link set ${LOCAL_WIFI_NET_DEVICE} up
+  brctl addbr ${LOCAL_BRIDGE_NET_DEVICE}
   brctl addif ${LOCAL_BRIDGE_NET_DEVICE} ${LOCAL_WIFI_NET_DEVICE}
   ip addr flush dev ${LOCAL_WIRE_NET_DEVICE}
   brctl addif ${LOCAL_BRIDGE_NET_DEVICE} ${LOCAL_WIRE_NET_DEVICE}
@@ -21,6 +71,7 @@ build_bridge() {
 
 tear_down_bridge() {
   ip addr flush dev ${LOCAL_BRIDGE_NET_DEVICE}
+  ip link set ${LOCAL_BRIDGE_NET_DEVICE} down
   brctl delif ${LOCAL_BRIDGE_NET_DEVICE} ${LOCAL_WIFI_NET_DEVICE}
   iw dev ${LOCAL_WIFI_NET_DEVICE} set 4addr off
   brctl delif ${LOCAL_BRIDGE_NET_DEVICE} ${LOCAL_WIRE_NET_DEVICE}
@@ -43,7 +94,9 @@ tear_down_iptables() {
 start() {
   build_bridge
   setup_iptables
+  generate_hostapd_conf
   service hostapd start
+  generate_dnsmasq_conf
   service dnsmasq start
 }
 
